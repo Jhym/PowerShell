8465abd3d8902ee750a7b4aa44e1afa900f858ae
Jhym <jatwillis@live.com>
2016-03-22 03:57:51 -0400

    Active Directory Computer Object Search
    Use this script to search for multiple computer objects in Active
    Directory. The script may need to be modified based on the name and
    location of the text file containing the machine names being queried.

---
+++ AD-ComputerRemoval_v1.0.ps1
@@ -0,0 +1,14 @@
+﻿#REMOVING MULTIPLE COMPUTERS FROM ACTIVE DIRECTORY
+#
+# Use this script to remove multiple computers from Active Directory.
+#
+#You may need to modify the script's Path search to reflect where the text file
+#with computer names is located.
+#
+#Currently the script looks for a folder labeled Tools in the root of C:
+#C:\Tools\ADComputerName.txt
+#
+#You will be prompted on if you wish to delete the AD objects.
+#
+#Authored by James Willis
+Get-Content -Path C:\Tools\ADComputerName.txt | Get-ADComputer | Remove-ADComputer
