e55808df1c7556fe4ce7eaa5eeef91fef8437422
Jhym <jatwillis@live.com>
2016-08-01 10:31:30 -0400

    Update and rename AD-ComputerSearch_v1.0.ps1 to AD-ComputerSearch_v1.1.ps1

---
+++ AD-ComputerSearch_v1.1.ps1
@@ -0,0 +1,12 @@
+#Searching for multiple computers from Active Directory
+#
+#You may need to modify the script's Path search to reflect where the text file
+#with the computer names is located.
+#
+#Currently the script looks for a folder labeled Tools in the root of C:
+#C:\Tools\ADComputerName.txt
+#
+#Authored by James Willis
+
+Get-Content -Path c:\tools\ADComputerName.txt | Get-ADComputer -Properties * | Format-Table 
+-Property CN, LastLogonDate, DistinguishedName -wrap
